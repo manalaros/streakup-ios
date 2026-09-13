b515ec93e1d31e5e8db5e610b458ef8e325d37b9
Manal Aros <manalaros@gmail.com>
2026-09-13 13:56:44 +0200

    Initial Commit

---
+++ StreakUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreakUp
+//
+//  Created by Manal Aros El Morabet on 13/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreakUp/StreakUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakUpApp.swift
+//  StreakUp
+//
+//  Created by Manal Aros El Morabet on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct StreakUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
